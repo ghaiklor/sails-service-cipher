032ee7e71e74fc60edcea10f62efd37136a7a843
Eugene Obrezkov <ghaiklor@gmail.com>
2015-06-16 19:52:54 +0300

    Add test for index

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,16 @@
 var assert = require('chai').assert;
 var CipherService = require('../');
+var JWTCipher = require('../lib/JwtCipher');
 
 describe('CipherFactory', function () {
   it('Should properly export', function () {
     assert.isObject(CipherService);
     assert.isFunction(CipherService.create);
     assert.isFunction(CipherService.JWT);
+  });
+
+  it('Should properly create JWT cipher', function () {
+    var cipher = CipherService.create('jwt');
+    assert.instanceOf(cipher, JWTCipher);
   });
 });
