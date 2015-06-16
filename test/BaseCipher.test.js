87fec26484b64e9bd3b82f45694f616a889a3022
Eugene Obrezkov <ghaiklor@gmail.com>
2015-06-16 18:26:29 +0300

    Write more tests

--- test/BaseCipher.test.js
+++ test/BaseCipher.test.js
@@ -3,12 +3,29 @@ var BaseCipher = require('../lib/BaseCipher');
 
 describe('BaseCipher', function () {
   it('Should properly export', function () {
-    assert.isObject(BaseCipher);
-    assert.isFunction(BaseCipher.prototype.getData);
-    assert.isFunction(BaseCipher.prototype.setData);
+    assert.isFunction(BaseCipher);
+    assert.isFunction(BaseCipher.prototype.getConfig);
+    assert.isFunction(BaseCipher.prototype.setConfig);
     assert.isFunction(BaseCipher.prototype.encode);
     assert.isFunction(BaseCipher.prototype.encodeSync);
     assert.isFunction(BaseCipher.prototype.decode);
     assert.isFunction(BaseCipher.prototype.decodeSync);
+  });
+
+  it('Should properly make objects configurable', function () {
+    var cipher = new BaseCipher();
+
+    assert.notOk(cipher.getConfig('foo'));
+    assert.instanceOf(cipher.setConfig('foo', 'bar'), BaseCipher);
+    assert.equal(cipher.getConfig('foo'), 'bar');
+  });
+
+  it('Should properly create cipher with pre-defined config', function () {
+    var cipher = new BaseCipher({
+      foo: 'bar'
+    });
+
+    assert.equal(cipher.getConfig('foo'), 'bar');
+    assert.notOk(cipher.getConfig('NOT_EXISTS'));
   });
 });
--- test/JwtCipher.test.js
+++ test/JwtCipher.test.js
@@ -1,7 +1,60 @@
-var assert = require('assert');
+var assert = require('chai').assert;
+var JwtCipher = require('../lib/JwtCipher');
 
 describe('JwtCipher', function () {
-  it('TODO', function () {
-    assert(true);
-  })
+  it('Should properly export', function () {
+    assert.isFunction(JwtCipher);
+  });
+
+  it('Should properly get/set secret key', function () {
+    var cipher = new JwtCipher();
+
+    assert.equal(cipher.getSecretKey(), 'DEFAULT_SECRET_KEY');
+    assert.instanceOf(cipher.setSecretKey('NEW_KEY'), JwtCipher);
+    assert.equal(cipher.getSecretKey(), 'NEW_KEY');
+  });
+
+  it('Should properly get/set algorithm', function () {
+    var cipher = new JwtCipher();
+
+    assert.equal(cipher.getAlgorithm(), 'HS512');
+    assert.instanceOf(cipher.setAlgorithm('HS256'), JwtCipher);
+    assert.equal(cipher.getAlgorithm(), 'HS256');
+  });
+
+  it('Should properly get/set expires in minutes', function () {
+    var cipher = new JwtCipher();
+
+    assert.equal(cipher.getExpiresInMinutes(), 60 * 24);
+    assert.instanceOf(cipher.setExpiresInMinutes(200), JwtCipher);
+    assert.equal(cipher.getExpiresInMinutes(), 200);
+  });
+
+  it('Should properly create cipher with default options', function () {
+    var cipher = new JwtCipher();
+
+    assert(cipher.getSecretKey(), 'DEFAULT_SECRET_KEY');
+    assert(cipher.getAlgorithm(), 'HS512');
+    assert(cipher.getExpiresInMinutes(), 60 * 24);
+  });
+
+  it('Should properly create cipher with custom options', function () {
+    var cipher = new JwtCipher({
+      secretKey: 'SECRET',
+      algorithm: 'HS256',
+      expiresInMinutes: 200
+    });
+
+    assert.equal(cipher.getSecretKey(), 'SECRET');
+    assert.equal(cipher.getAlgorithm(), 'HS256');
+    assert.equal(cipher.getExpiresInMinutes(), 200);
+  });
+
+  it('Should properly encode data', function () {
+
+  });
+
+  it('Should properly decode data', function () {
+
+  });
 });
