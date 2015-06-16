b5d829186879600d5b693c275ad3038e65e4a8d9
Eugene Obrezkov <ghaiklor@gmail.com>
2015-06-16 17:58:31 +0300

    Done with some restructuring

--- test/BaseCipher.test.js
+++ test/BaseCipher.test.js
@@ -1,7 +1,14 @@
-var assert = require('assert');
+var assert = require('chai').assert;
+var BaseCipher = require('../lib/BaseCipher');
 
 describe('BaseCipher', function () {
-  it('TODO', function () {
-    assert(true);
-  })
+  it('Should properly export', function () {
+    assert.isObject(BaseCipher);
+    assert.isFunction(BaseCipher.prototype.getData);
+    assert.isFunction(BaseCipher.prototype.setData);
+    assert.isFunction(BaseCipher.prototype.encode);
+    assert.isFunction(BaseCipher.prototype.encodeSync);
+    assert.isFunction(BaseCipher.prototype.decode);
+    assert.isFunction(BaseCipher.prototype.decodeSync);
+  });
 });
